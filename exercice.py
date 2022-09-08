48eff2c9d8e1c2f10fea541740c2d05620937057
Vincent Drolet <1437149@etu.cegep-lanaudiere.qc.ca>
2022-09-08 16:19:32 -0400

    Premier essai exercices 3.1

--- exercice.py
+++ exercice.py
@@ -4,32 +4,36 @@
 
 import math
 
+
 def square_root(a: float) -> float:
-    return 0.0
+    return a ** (1 / 2)
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a ** 2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return (angle_degs / 180 + angle_mins / (180 * 60) + angle_secs / (180 * 60 * 60)) * 3.141592653
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    degrees = angle_rads * 180 / 3.141592653
+    minutes = (degrees - floor(degrees)) * 60
+    seconds = (minutes - floor(minutes)) * 60
+    return floor(degrees), floor(minutes), seconds
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) * 5 / 9
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature * 9 / 5) + 32
 
 
 def main() -> None:
